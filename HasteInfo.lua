d5a134c55c813a6ac203eff84807dc8a647c8746
shastaxc <shastaxc@gmail.com>
2022-12-04 13:53:51 -0500

    Version bump

--- HasteInfo.lua
+++ HasteInfo.lua
@@ -1,6 +1,6 @@
 _addon.name = 'HasteInfo'
 _addon.author = 'Shasta'
-_addon.version = '0.0.4'
+_addon.version = '0.0.5'
 _addon.commands = {'hi','hasteinfo'}
 
 -------------------------------------------------------------------------------
